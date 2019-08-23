07efa545770d01d58c938a1179a69cc74d725796
GQSM <ms314006@hotmail.com>
2019-08-23 22:28:44 +0800

    [ADD] 為整個網站預設字體

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Typography from '@material-ui/core/Typography';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import Main from './component/Main';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Consolas, monaco, monospace',
+  },
+});
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <Typography>
+      <Main />
+    </Typography>
+  </ThemeProvider>,
+  document.getElementById('root')
+);
